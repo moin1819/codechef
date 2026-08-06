edd53475d02a767282420b7ddedd2307ad299c40
Mohammed Moin <starboy.moin1926@gmail.com>
2026-08-06 06:48:01 +0530

    ✅ LJAAS109 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-06

---
+++ codechef/medium/LJAAS109/solution.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int num = 1;
+        while (num <= 5) {
+            System.out.print((num * num) + " ");
+            num++;
+        }
+    }
+}
