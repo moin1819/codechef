68c2c3198a2f090cc47ce70574be17b1dbb9100c
Mohammed Moin <starboy.moin1926@gmail.com>
2026-08-08 23:03:06 +0530

    ✅ Find the Lexicographically Smallest Valid Sequence | Medium | Python | Runtime: 584 ms (47.83%) | Memory: 47.3 MB | 2026-08-08

--- leetcode/medium/3302-find-the-lexicographically-smallest-valid-sequence/solution.py
+++ leetcode/medium/3302-find-the-lexicographically-smallest-valid-sequence/solution.py
@@ -1,9 +1,7 @@
 class Solution:
     def validSequence(self, word1: str, word2: str) -> list[int]:
         n1, n2 = len(word1), len(word2)
-        
-        # suffix[j] stores the largest index in word1 from which 
-        # word2[j:] can be matched EXACTLY as a subsequence.
+    
         suffix = [-1] * n2
         p1 = n1 - 1
         for j in range(n2 - 1, -1, -1):
