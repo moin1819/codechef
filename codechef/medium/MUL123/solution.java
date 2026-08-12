f67584eb4b3f12c0394c910dd1526e168f0fb860
Mohammed Moin <starboy.moin1926@gmail.com>
2026-08-12 20:26:32 +0530

    ✅ MUL123 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-12

--- codechef/medium/MUL123/solution.java
+++ codechef/medium/MUL123/solution.java
@@ -1,12 +1,24 @@
-import java.util.*;
-import java.lang.*;
-import java.io.*;
+import java.util.Scanner;
 
-class Codechef
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		// your code goes here
-
-	}
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        if (sc.hasNextInt()) {
+            int T = sc.nextInt();
+            while (T-- > 0) {
+                int N = sc.nextInt();
+                if (N % 3 == 0) {
+                    System.out.println(0);
+                } else {
+                    int nextMult5 = N + (5 - (N % 5));
+                    if ((N + 1) % 3 == 0 || nextMult5 % 3 == 0) {
+                        System.out.println(1);
+                    } else {
+                        System.out.println(2);
+                    }
+                }
+            }
+        }
+        sc.close();
+    }
 }
