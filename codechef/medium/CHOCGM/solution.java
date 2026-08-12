bf5b6e20f04737cadc5f3ffb816c656d5d1483f2
Mohammed Moin <starboy.moin1926@gmail.com>
2026-08-12 20:27:21 +0530

    ✅ CHOCGM | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-12

--- codechef/medium/CHOCGM/solution.java
+++ codechef/medium/CHOCGM/solution.java
@@ -1,12 +1,32 @@
-import java.util.*;
-import java.lang.*;
-import java.io.*;
+import java.util.Scanner;
 
-class Codechef
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		// your code goes here
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        if (sc.hasNextInt()) {
+            int T = sc.nextInt();
+            while (T-- > 0) {
+                int N = sc.nextInt();
+                long[] A = new long[N];
+                long totalSum = 0;
 
-	}
+                for (int i = 0; i < N; i++) {
+                    A[i] = sc.nextLong();
+                    totalSum += A[i];
+                }
+
+                if (totalSum % 2 != 0) {
+                    System.out.println(0);
+                } else {
+                    long aliceChocolates = 0;
+                    for (int i = 0; i < N; i++) {
+                        aliceChocolates += A[i] - (A[i] % 2);
+                    }
+                    aliceChocolates += 1;
+                    System.out.println(aliceChocolates);
+                }
+            }
+        }
+        sc.close();
+    }
 }
