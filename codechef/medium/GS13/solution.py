27fd58686fcadeaf31f6301a969e8f79f76fc326
Mohammed Moin <starboy.moin1926@gmail.com>
2026-08-26 10:25:15 +0530

    ✅ GS13 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-26

---
+++ codechef/medium/GS13/solution.py
@@ -0,0 +1,10 @@
+# Accept the number of test cases
+t = int(input())
+
+# Loop through each test case
+for _ in range(t):
+    # Read the integer N for the current test case
+    n = int(input())
+    
+    # Output the number with its sign flipped
+    print(-n)
