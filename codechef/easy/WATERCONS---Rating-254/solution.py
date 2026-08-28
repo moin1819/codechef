8706c9acf37ce31dda7b1cd5fc3bc64cc08be7ae
Mohammed Moin <starboy.moin1926@gmail.com>
2026-08-28 23:28:40 +0530

    ✅ WATERCONS - Rating 254 | Easy | Python | Runtime: N/A | Memory: N/A | 2026-08-28

--- codechef/easy/WATERCONS---Rating-254/solution.py
+++ codechef/easy/WATERCONS---Rating-254/solution.py
@@ -1,20 +1,9 @@
-import sys
+T = int(input())
 
-def main():
-    input_data = sys.stdin.read().split()
-    if not input_data:
-        return
-    
-    T = int(input_data[0])
-    
-    for i in range(1, T + 1):
-        X = int(input_data[i])
-        
-        # Check if Chef drank at least 2000 ml
-        if X >= 2000:
-            print("YES")
-        else:
-            print("NO")
+for _ in range(T):
+    X = int(input())
 
-if __name__ == '__main__':
-    main()
+    if X >= 2000:
+        print("YES")
+    else:
+        print("NO")
