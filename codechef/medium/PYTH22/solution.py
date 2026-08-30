303e8bd7f7f17a6efcae5fdf8bf194095431fa5c
Mohammed Moin <starboy.moin1926@gmail.com>
2026-08-30 21:47:04 +0530

    ✅ PYTH22 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-30

--- codechef/medium/PYTH22/solution.py
+++ codechef/medium/PYTH22/solution.py
@@ -1,10 +1,4 @@
-# Incorrect (causes SyntaxError):
-# 1st_word = "Code"
-# 2nd_word = "Chef"
-# print(1st_word, 2nd_word)
+a = 23
+b = 20
 
-# Fixed Code:
-first_word = "Code"
-second_word = "Chef"
-
-print(first_word, second_word)
+print(a + b)
