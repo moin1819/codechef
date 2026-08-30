e543ceae03e08e608dc43680e5920c1b19e96829
Mohammed Moin <starboy.moin1926@gmail.com>
2026-08-30 21:46:34 +0530

    ✅ VAR5 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-30

---
+++ codechef/medium/VAR5/solution.py
@@ -0,0 +1,10 @@
+# Incorrect (causes SyntaxError):
+# 1st_word = "Code"
+# 2nd_word = "Chef"
+# print(1st_word, 2nd_word)
+
+# Fixed Code:
+first_word = "Code"
+second_word = "Chef"
+
+print(first_word, second_word)
