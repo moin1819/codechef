4a929187f87e1d35b1c4d3568d6bc28f348c6bbb
Mohammed Moin <starboy.moin1926@gmail.com>
2026-09-01 21:21:47 +0530

    ✅ PYTH32 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-01

---
+++ codechef/medium/PYTH32/solution.py
@@ -0,0 +1,11 @@
+# Initialize variables
+side = 14
+cost_per_sq_cm = 7
+
+# Compute area and cost
+area = side * side
+cost = area * cost_per_sq_cm
+
+# Output the results on separate lines
+print(area)
+print(cost)
