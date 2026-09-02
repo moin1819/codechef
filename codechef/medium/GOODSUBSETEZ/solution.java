8c114ffb58ff9de0b9158650b8ff912d3236e5cf
Mohammed Moin <starboy.moin1926@gmail.com>
2026-09-02 21:07:16 +0530

    ✅ GOODSUBSETEZ | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-02

--- codechef/medium/GOODSUBSETEZ/solution.java
+++ codechef/medium/GOODSUBSETEZ/solution.java
@@ -1,12 +1,34 @@
-import java.util.*;
-import java.lang.*;
-import java.io.*;
+import java.util.Scanner;
 
-class Codechef
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		// your code goes here
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-	}
+        int T = sc.nextInt();
+
+        while (T-- > 0) {
+            int N = sc.nextInt();
+
+            int[] count = new int[31];
+
+            for (int i = 0; i < N; i++) {
+                int x = sc.nextInt();
+
+                // Find position of highest set bit
+                int highestBit = 31 - Integer.numberOfLeadingZeros(x);
+
+                count[highestBit]++;
+            }
+
+            int answer = 0;
+
+            for (int i = 0; i < 31; i++) {
+                answer = Math.max(answer, count[i]);
+            }
+
+            System.out.println(answer);
+        }
+
+        sc.close();
+    }
 }
