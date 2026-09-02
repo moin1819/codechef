fe8e5ab3efbcf70dcb2b731c061a630e7ed51cf4
Mohammed Moin <starboy.moin1926@gmail.com>
2026-09-02 21:04:57 +0530

    ✅ MAXSUM77 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-02

--- codechef/medium/MAXSUM77/solution.java
+++ codechef/medium/MAXSUM77/solution.java
@@ -1,12 +1,37 @@
-import java.util.*;
-import java.lang.*;
-import java.io.*;
+import java.util.Scanner;
 
-class Codechef
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		// your code goes here
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-	}
+        int T = sc.nextInt();
+
+        while (T-- > 0) {
+            int N = sc.nextInt();
+            int K = sc.nextInt();
+            int[] A = new int[N];
+            for (int i = 0; i < N; i++) {
+                A[i] = sc.nextInt();
+            }
+
+            int len = N - K;
+            int currentSum = 0;
+            for (int i = 0; i < len; i++) {
+                currentSum += A[i];
+            }
+
+            int maxSum = currentSum;
+
+            for (int i = len; i < N; i++) {
+                currentSum += A[i];
+                currentSum -= A[i - len];
+
+                maxSum = Math.max(maxSum, currentSum);
+            }
+
+            System.out.println(maxSum);
+        }
+
+        sc.close();
+    }
 }
