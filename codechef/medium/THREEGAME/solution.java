553e0a1ee82ee6380c1c51e9cf83dbd42b72ff33
Mohammed Moin <starboy.moin1926@gmail.com>
2026-09-02 21:08:28 +0530

    ✅ THREEGAME | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-02

--- codechef/medium/THREEGAME/solution.java
+++ codechef/medium/THREEGAME/solution.java
@@ -1,12 +1,21 @@
-import java.util.*;
-import java.lang.*;
-import java.io.*;
+import java.util.Scanner;
 
-class Codechef
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		// your code goes here
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-	}
+        int T = sc.nextInt();
+
+        while (T-- > 0) {
+            long N = sc.nextLong();
+
+            if (N == 1) {
+                System.out.println(1);
+            } else {
+                System.out.println(2 * N);
+            }
+        }
+
+        sc.close();
+    }
 }
