c979e06d84d7e8c5e61fecaa18aa448b9b1ec1be
Mohammed Moin <starboy.moin1926@gmail.com>
2026-09-02 21:00:11 +0530

    ✅ ELECTIONS - Rating 1034 | Easy | Python | Runtime: N/A | Memory: N/A | 2026-09-02

---
+++ codechef/easy/ELECTIONS---Rating-1034/solution.py
@@ -0,0 +1,13 @@
+T = int(input())
+
+for _ in range(T):
+    XA, XB, XC = map(int, input().split())
+
+    if XA > 50:
+        print("A")
+    elif XB > 50:
+        print("B")
+    elif XC > 50:
+        print("C")
+    else:
+        print("NOTA")
