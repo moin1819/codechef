819bf91db08a50fea8c775cb62db99396f97933d
Mohammed Moin <starboy.moin1926@gmail.com>
2026-09-02 21:04:18 +0530

    ✅ REACHWT | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-02

--- codechef/medium/REACHWT/solution.java
+++ codechef/medium/REACHWT/solution.java
@@ -1,12 +1,23 @@
-import java.util.*;
-import java.lang.*;
-import java.io.*;
+import java.util.Scanner;
 
-class Codechef
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		// your code goes here
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-	}
+        int T = sc.nextInt();
+
+        while (T-- > 0) {
+            int N = sc.nextInt();
+
+            int cost = (N / 2) * 30;
+
+            if (N % 2 != 0) {
+                cost += 20;
+            }
+
+            System.out.println(cost);
+        }
+
+        sc.close();
+    }
 }
