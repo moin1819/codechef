bd7f387bc1a567a307e8b6f2e6b7ef39da9cce2a
Mohammed Moin <starboy.moin1926@gmail.com>
2026-09-09 21:41:41 +0530

    ✅ EGGBUY | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-09

--- codechef/medium/EGGBUY/solution.java
+++ codechef/medium/EGGBUY/solution.java
@@ -1,12 +1,15 @@
-import java.util.*;
-import java.lang.*;
-import java.io.*;
+import java.util.Scanner;
 
-class Codechef
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		// your code goes here
-
-	}
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int X = sc.nextInt();
+        int Y = sc.nextInt();
+        int F = sc.nextInt();
+        
+        int cost1 = 12 * X;
+        int cost2 = 12 * Y + F;
+        
+        System.out.println(Math.min(cost1, cost2));
+    }
 }
