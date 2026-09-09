3e8414191ad675f605b4f9b26281ef2cf7458330
Mohammed Moin <starboy.moin1926@gmail.com>
2026-09-09 21:44:12 +0530

    ✅ ADDIS | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-09

--- codechef/medium/ADDIS/solution.java
+++ codechef/medium/ADDIS/solution.java
@@ -1,12 +1,30 @@
 import java.util.*;
-import java.lang.*;
-import java.io.*;
 
-class Codechef
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		// your code goes here
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-	}
+        int T = sc.nextInt();
+
+        while (T-- > 0) {
+            int N = sc.nextInt();
+
+            HashMap<Integer, Integer> freq = new HashMap<>();
+
+            int maxFreq = 0;
+
+            for (int i = 0; i < N; i++) {
+                int x = sc.nextInt();
+
+                int count = freq.getOrDefault(x, 0) + 1;
+                freq.put(x, count);
+
+                maxFreq = Math.max(maxFreq, count);
+            }
+
+            System.out.println((maxFreq + 1) / 2);
+        }
+
+        sc.close();
+    }
 }
