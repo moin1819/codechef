d82ef3ec2952a890bbb52060e3317d36c0afb5dd
Mohammed Moin <starboy.moin1926@gmail.com>
2026-09-09 21:46:53 +0530

    ✅ BTCMINE | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-09

--- codechef/medium/BTCMINE/solution.java
+++ codechef/medium/BTCMINE/solution.java
@@ -7,24 +7,20 @@ class Main {
         int T = sc.nextInt();
 
         while (T-- > 0) {
-            int X = sc.nextInt();
-            int Y = sc.nextInt();
+            long X = sc.nextLong();
+            long Y = sc.nextLong();
 
             long profit = 0;
-            int gpu = 0;
+            long gpu = 0;
             int days = 0;
 
             while (profit <= 0) {
                 days++;
-
-                // Buy one GPU
-                gpu++;
-
-                // Mining income for this day
-                profit += (long) Y * gpu * gpu;
-
-                // Cost of GPU
-                profit -= X;
+                if (Y * (2 * gpu + 1) > X) {
+                    gpu++;
+                    profit -= X;
+                }
+                profit += Y * gpu * gpu;
             }
 
             System.out.println(days);
