2294c3f4b718e7400108040ba80f3fe43a7b23e1
Mohammed Moin <starboy.moin1926@gmail.com>
2026-09-09 21:43:08 +0530

    ✅ UNQSHF | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-09

--- codechef/medium/UNQSHF/solution.java
+++ codechef/medium/UNQSHF/solution.java
@@ -1,12 +1,38 @@
 import java.util.*;
-import java.lang.*;
-import java.io.*;
 
-class Codechef
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		// your code goes here
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-	}
+        int T = sc.nextInt();
+
+        while (T-- > 0) {
+            int N = sc.nextInt();
+            String A = sc.next();
+            String B = sc.next();
+
+            int countA = 0;
+            int countB = 0;
+
+            for (char c : A.toCharArray()) {
+                if (c == 'a') {
+                    countA++;
+                }
+            }
+
+            for (char c : B.toCharArray()) {
+                if (c == 'a') {
+                    countB++;
+                }
+            }
+
+            if (countA + countB == N) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+
+        sc.close();
+    }
 }
