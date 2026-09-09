d1f0b8d89df38aed260c494bedb17aeec89f5812
Mohammed Moin <starboy.moin1926@gmail.com>
2026-09-09 21:44:56 +0530

    ✅ BTCMINE | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-09

--- codechef/medium/BTCMINE/solution.java
+++ codechef/medium/BTCMINE/solution.java
@@ -1,12 +1,35 @@
 import java.util.*;
-import java.lang.*;
-import java.io.*;
 
-class Codechef
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		// your code goes here
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-	}
+        int T = sc.nextInt();
+
+        while (T-- > 0) {
+            int X = sc.nextInt();
+            int Y = sc.nextInt();
+
+            long profit = 0;
+            int gpu = 0;
+            int days = 0;
+
+            while (profit <= 0) {
+                days++;
+
+                // Buy one GPU
+                gpu++;
+
+                // Mining income for this day
+                profit += (long) Y * gpu * gpu;
+
+                // Cost of GPU
+                profit -= X;
+            }
+
+            System.out.println(days);
+        }
+
+        sc.close();
+    }
 }
