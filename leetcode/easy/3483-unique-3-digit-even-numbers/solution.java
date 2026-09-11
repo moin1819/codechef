621449f8e985da4483197e182da90a83c1ec8afa
Mohammed Moin <starboy.moin1926@gmail.com>
2026-09-11 19:26:52 +0530

    ✅ Unique 3-Digit Even Numbers | Easy | Java | Runtime: 11 ms (19.47%) | Memory: 46.8 MB | 2026-09-11

---
+++ leetcode/easy/3483-unique-3-digit-even-numbers/solution.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int totalNumbers(int[] digits) {
+        int[] freq = new int[10];
+        for (int d : digits) {
+            freq[d]++;
+        }
+
+        int answer = 0;
+        for (int num = 100; num <= 999; num++) {
+
+            if (num % 2 != 0) {
+                continue;
+            }
+
+            int a = num / 100;
+            int b = (num / 10) % 10;
+            int c = num % 10;
+            int[] need = new int[10];
+            need[a]++;
+            need[b]++;
+            need[c]++;
+
+            boolean possible = true;
+
+            for (int d = 0; d <= 9; d++) {
+                if (need[d] > freq[d]) {
+                    possible = false;
+                    break;
+                }
+            }
+            if (possible) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
