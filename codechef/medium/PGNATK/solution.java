d568f3e5c0e8eef85741dff3e8c9ddfe6971b659
Mohammed Moin <starboy.moin1926@gmail.com>
2026-09-23 21:37:22 +0530

    ✅ PGNATK | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-23

--- codechef/medium/PGNATK/solution.java
+++ codechef/medium/PGNATK/solution.java
@@ -1,12 +1,19 @@
 import java.util.*;
-import java.lang.*;
-import java.io.*;
 
-class Codechef
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		// your code goes here
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-	}
+        int T = sc.nextInt();
+
+        while (T-- > 0) {
+            int N = sc.nextInt();
+            int K = sc.nextInt();
+
+            // Every K-th minute is a delay.
+            int delays = (N - 1) / (K - 1);
+
+            System.out.println(N + delays);
+        }
+    }
 }
