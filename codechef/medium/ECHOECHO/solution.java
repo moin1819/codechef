b296749db25c12e4cd2c235f743ca1e2a898a734
Mohammed Moin <starboy.moin1926@gmail.com>
2026-09-23 21:34:28 +0530

    ✅ ECHOECHO | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-23

--- codechef/medium/ECHOECHO/solution.java
+++ codechef/medium/ECHOECHO/solution.java
@@ -1,12 +1,14 @@
 import java.util.*;
-import java.lang.*;
-import java.io.*;
 
-class Codechef
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		// your code goes here
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
 
-	}
+        if (s.charAt(0) == s.charAt(2) && s.charAt(1) == s.charAt(3)) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
 }
